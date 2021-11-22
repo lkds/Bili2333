0a5416a51c66bd709e32b528b7a4abf895c03c62
fnzs <fnzs@foxmail.com>
2021-11-22 22:12:04 +0800

    add: echarts

--- src/components/Point.vue
+++ src/components/Point.vue
@@ -1,6 +1,50 @@
-<script setup></script>
+<script setup>
+
+</script>
 <template>
-<div>
-    定点
-</div>
+    <div>
+        <div id="t1" style="height:100%; width:100%"></div>
+        <div id="t2" style="height:100%; width:100%"></div>
+    </div>
 </template>
+<script>
+import { getCurrentInstance } from 'vue';
+export default {
+    name: 'Point',
+    mounted() {
+        this.get_data()
+    },
+    data() {
+        return {
+            init_data: {
+                xAxis: {
+                    data: ['A', 'B', 'C', 'D', 'E']
+                },
+                yAxis: {},
+                series: [
+                    {
+                        data: [10, 22, 28, 43, 49],
+                        type: 'bar',
+                        stack: 'x'
+                    },
+                    {
+                        data: [5, 4, 3, 5, 10],
+                        type: 'bar',
+                        stack: 'x'
+                    }
+                ]
+            }
+        };
+    },
+    methods: {
+        get_data() {
+            const { proxy } = getCurrentInstance()
+            console.log(proxy)
+            let mycharts = proxy.$echarts.init(document.getElementById('t1'))
+            let mycharts2 = proxy.$echarts.init(document.getElementById('t2'))
+            mycharts.setOption(this.init_data);
+            mycharts2.setOption(this.init_data);
+        }
+    },
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import ElementPlus from 'element-plus'
+import * as echarts from 'echarts'
 import 'element-plus/dist/index.css'
 const app = createApp(App)
 app.use(ElementPlus)
 app.use(router)
+app.config.globalProperties.$echarts = echarts
 app.mount('#app')
